dec2bdce1219b38c6788ffb285487efef86b6b84
Artem Yankovskiy <artemyankovskiy@gmail.com>
2014-04-01 13:23:14 +1100

    Initial creation

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 PluginExample
 =============
 
-Example plugin for PhotoTools
+Example plug-in for Photo Tools
+
+Use this example for develop your plug-in.
---
+++ src/com/example/pluginexample/PluginActivity.java
@@ -0,0 +1,14 @@
+package com.example.pluginexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PluginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plugin_activity);
+    }
+    
+}
